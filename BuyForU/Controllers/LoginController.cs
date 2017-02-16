41dd9a8b6b65e872e7a9909b322e06b8092d5eca
906 <906@TOPAZ15>
2017-02-16 16:58:42 +0200

    Add project files.

---
+++ BuyForU/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using BuyForU.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuyForU.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            using (DB context = new DB())
+            {
+                User tmp = context.Users.Where(u => u.UserName == user.UserName
+                    && u.Password == user.Password).FirstOrDefault();
+                if (tmp != null)
+                {
+                    System.Web.Security.FormsAuthentication.SetAuthCookie
+                        ($"{tmp.FirstName} {tmp.LastName}", true);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BuyForU/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BuyForU.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        [Required (ErrorMessage = "User Name Is Required")]
+        public string UserName { get; set; }
+        [Required (ErrorMessage = "Password Is Required")]
+        public string Password { get; set; }
+    }
+}
